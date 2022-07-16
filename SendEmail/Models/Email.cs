2208ca2d52e82ff644a675c8d5068f21ff9a021b
ahmdmhmd1907 <ahmdmhmd1907@gmail.com>
2022-07-16 19:34:31 +0200

    Add project files.

---
+++ SendEmail/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmail.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IFormFile Attachment { get; set; }
+        public string Gmail { get; set; }
+        public string Password { get; set; }
+    }
+}
